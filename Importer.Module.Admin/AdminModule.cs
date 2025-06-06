8799fcce0e05e76c131681b9c87aae6ff4f37b3c
Kevin Spruill <kevin@mmlabel.com>
2025-06-06 12:51:06 -0700

    Add Importer.Module.Admin project and related files
    - Introduced a new project "Importer.Module.Admin" in the solution.
    - Created `AdminModule.cs` implementing `IImporterModule` with unimplemented methods.
    - Added project file `Importer.Module.Admin.csproj` with configurations and references.
    - Created `AssemblyInfo.cs` for assembly metadata.
    - Updated solution to include the new Admin module in global sections.

---
+++ Importer.Module.Admin/AdminModule.cs
@@ -0,0 +1,65 @@
+﻿using Importer.Common.Interfaces;
+using Importer.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Importer.Module.Admin
+{
+    public class AdminModule : IImporterModule
+    {
+        public string Name { get; set; } = "Admin";
+        public string Version { get; set; } = "1.0.0";
+        public ImporterInstance ImporterInstance { get; set; }
+        public tblProducts ProductTemplate { get; set; } = new tblProducts();
+        public bool Flush { get; set; }
+        public string ImporterTypeData { get; set; } = string.Empty;
+
+        public AdminModule()
+        {
+            
+        }
+
+        public int GetPendingFileCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<tblProducts> GetTblProductsDeleteList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<tblProducts> GetTblProductsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitModule(ImporterInstance importerInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetupImporterType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartModule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopModule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TriggerProcess()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
